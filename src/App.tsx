391eab0026b2ba62789fd850c8ed9c9643063106
ANEUP002 <146136473+ANEUP002@users.noreply.github.com>
2023-10-25 14:31:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Message from "./message";
 
 function App() {
-  return <div>Here comes the message <Message /></div>;
+  return (
+  <div>Here comes the message <Message /></div>);
 }
 
 export default App;
